ae09eed2181d2bfeab4ab8d93fc487216eedc9f7
Elias Mawa <elias@emawa.io>
2021-11-07 21:41:08 -0700

    refactor: add authority level to role schema

--- prisma/migrations/20211108042354_init/migration.sql
+++ prisma/migrations/20211108042354_init/migration.sql
@@ -35,8 +35,10 @@ CREATE TABLE `Folder` (
 -- CreateTable
 CREATE TABLE `Role` (
     `roleId` VARCHAR(16) NOT NULL,
+    `authority` TINYINT UNSIGNED NOT NULL,
 
     UNIQUE INDEX `Role_roleId_key`(`roleId`),
+    UNIQUE INDEX `Role_authority_key`(`authority`),
     PRIMARY KEY (`roleId`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
