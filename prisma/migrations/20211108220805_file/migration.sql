df4a565062b672736f75050ad129a40ebefe7315
Elias Mawa <elias@emawa.io>
2021-11-20 20:05:44 -0700

    featureL media col & deafault for veiws/dl

--- prisma/migrations/20211108220805_file/migration.sql
+++ prisma/migrations/20211108220805_file/migration.sql
@@ -6,19 +6,19 @@ CREATE TABLE `File` (
     `fileId` VARCHAR(16) NOT NULL,
     `folderId` VARCHAR(16) NOT NULL,
     `name` VARCHAR(255) NOT NULL,
-    `type` VARCHAR(32) NOT NULL,
+    `media` BOOLEAN NOT NULL DEFAULT false,
+    `type` VARCHAR(255) NOT NULL,
     `ext` VARCHAR(32) NOT NULL,
     `bytes` INTEGER UNSIGNED NOT NULL,
     `sha256` VARCHAR(64) NOT NULL,
     `md5` VARCHAR(32) NOT NULL,
-    `dCount` INTEGER UNSIGNED NOT NULL,
-    `vCount` INTEGER UNSIGNED NOT NULL,
+    `dCount` INTEGER UNSIGNED NOT NULL DEFAULT 0,
+    `vCount` INTEGER UNSIGNED NOT NULL DEFAULT 0,
     `deleted` DATETIME(3) NULL,
     `created` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated` DATETIME(3) NOT NULL,
 
     UNIQUE INDEX `File_fileId_key`(`fileId`),
-    UNIQUE INDEX `File_folderId_key`(`folderId`),
     PRIMARY KEY (`fileId`, `folderId`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
