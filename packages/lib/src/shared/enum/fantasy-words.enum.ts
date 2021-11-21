2b9dfc9d854abd3e2ce5ef40d964ba64b20aa851
Elias Mawa <elias@emawa.io>
2021-11-20 20:05:00 -0700

    feature: fantasy word generator

---
+++ packages/lib/src/shared/enum/fantasy-words.enum.ts
@@ -0,0 +1,29 @@
+import fs from 'fs';
+import path from 'path';
+import crypto from 'crypto';
+
+const adjective = fs.readFileSync(path.resolve(__dirname, 'fantasy-words/adjective.txt'), 'utf8');
+const adverb = fs.readFileSync(path.resolve(__dirname, 'fantasy-words/adverb.txt'), 'utf8');
+const noun = fs.readFileSync(path.resolve(__dirname, 'fantasy-words/noun.txt'), 'utf8');
+
+export const FantasyAdjectives = adjective.split(/,/);
+export const FantasyAdverbs = adverb.split(/,/);
+export const FantasyNouns = noun.split(/,/);
+
+export const secureRandomNumber = (max: number) => {
+  return parseInt(crypto.randomBytes(4).toString('hex'), 16) % max;
+};
+
+export const getRandomFantasyWord = (list: string[]): string => {
+  return list[secureRandomNumber(list.length)];
+};
+
+export const generateFantasyName = (): string => {
+  const word = [
+    getRandomFantasyWord(FantasyAdverbs),
+    getRandomFantasyWord(FantasyAdjectives),
+    getRandomFantasyWord(FantasyAdjectives),
+    getRandomFantasyWord(FantasyNouns),
+  ];
+  return word.join('-');
+};
--- packages/lib/src/shared/enum/index.ts
+++ packages/lib/src/shared/enum/index.ts
@@ -1,2 +1,3 @@
+export * from './fantasy-words.enum';
 export * from './role.enum';
 export * from './http.enum';
